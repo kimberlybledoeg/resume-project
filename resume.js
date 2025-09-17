bd9ee7a3be84fe91bf4761e073e9bbfaafe5067b
kimberlybledoeg <fkbledoeg@oru.edu>
2025-09-17 09:19:03 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+//Define Variables
+/* */
+let myName = "Kimberly";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Student Manager, Worship Leader, and Student Ambassador";
+let skills = ["Finding good food", "Organization", "Time management"];
+
+//connect variables to HTML (ligaments)
+
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
