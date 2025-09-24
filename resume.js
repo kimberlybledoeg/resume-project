9768a3b4d91926a7ec40f32e945db58c8d051cd6
kimberlybledoeg <fkbledoeg@oru.edu>
2025-09-23 21:02:58 -0500

    resume.js

--- resume.js
+++ resume.js
@@ -3,20 +3,20 @@
 let myName = "Kimberly";
 let title = "Student";
 let education = "Oral Roberts University";
-let experience = "Student Sports Manager, Worship Leader, and Student Ambassador";
-let skills = ["Public Relations", "Organization", "Conflict Resolution"];
+let experience = ["Student Sports Manager", "Worship Leader", "Student Ambassador"];
 
 function showFunFact(){
     alert("Fun Fact: I love rollercoasters");
 
 //Creates an unordered list with the elements of the list 'skills'
-
+let skills = ["Public Relations", "Organization", "Conflict Resolution"];
+letskillsList=document.createElement("ul");
 for (let i = 0; i < skills.length; i++){
     let li = document.createElement('li');
     li.textContent = skills[i];
     skillsList.appendChild(li);
 }
-
+document.body.appendChild(skillsList);
 //connect variables to HTML (ligaments)
 document.getElementById("my-name").textContent = myName;
 document.getElementById("title").textContent = title;
